9b2264df09c41f7345bca0fe0f9ad49ffadca98c
Markus Stäuble <staeuble@gmail.com>
2020-04-15 11:19:49 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 input.onButtonPressed(Button.B, function () {
     taste_b_gedrueckt = 1
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
 })
 let taste_b_gedrueckt = 0
 taste_b_gedrueckt = 0
 while (taste_b_gedrueckt == 0) {
     basic.showIcon(IconNames.Square)
-    basic.pause(100)
+    basic.pause(200)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -13,6 +15,7 @@ while (taste_b_gedrueckt == 0) {
         . . . . .
         . . . . .
         `)
+    basic.pause(200)
 }
 basic.forever(function () {
 	
